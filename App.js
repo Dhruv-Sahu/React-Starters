818d969f8d5080e9bdfa340d21f3e2a2f28368d1
Dhruv Sahu <82047955+Dhruv-Sahu@users.noreply.github.com>
2021-11-11 23:51:56 +0530

    app for pluckard

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
-import React,{useState,useEffect} from 'react';
-import './App.css';
-
-const quotes = [
-  "“Life is what happens when you’re busy making other plans.”",
-  "“Get busy living or get busy dying.”",
-  "“You only live once, but if you do it right, once is enough.”",
-  "“Many of life’s failures are people who did not realize how close they were to success when they gave up.”",
-  "“Never let the fear of striking out keep you from playing the game.”",
-  "“Money and success don’t change people; they merely amplify what is already there.”"
-]
+import React, { useState } from 'react'
+import data from './data'
+import SingleQuestion from './Question'
 
 function App() {
-  const [quote,setQuote] = useState(quotes[0]);
-
-  function randomizeQuote(){
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-    setQuote(randomQuote);
-  }
-
-  return <div className="App">
-      <h4>{quote}</h4>
-      <button onClick={randomizeQuote}>Click Me</button>
-  </div>
+    return (
+        <main>
+            <div className="container">
+                <h3>FAQ</h3>
+                <section className="info">
+                    {data.map((question) => {
+                        return (
+                            <SingleQuestion
+                                title={question.title}
+                                key={question.id}
+                            />
+                        )
+                    })}
+                </section>
+            </div>
+        </main>
+    )
 }
 
-export default App;
+export default App
